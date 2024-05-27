6ffe155073c8fb204a4ca1e0768aa9ed2f50ebca
thealphaboy1 <thealphaboy30@gmail.com>
2024-05-27 13:46:00 +0530

    Create wrapper.py

---
+++ SHUKLA/modules/helpers/wrapper.py
@@ -0,0 +1,113 @@
+from pyrogram.types import *
+from traceback import format_exc
+
+from ...console import SUDOERS
+from ..clients.clients import app, bot
+
+def super_user_only(mystic):
+    async def wrapper(client, message):
+        try:
+            if message.from_user.is_self:
+                return await mystic(client, message)
+        except:
+            if message.outgoing:
+                return await mystic(client, message)
+            
+    return wrapper
+
+
+
+def sudo_users_only(mystic):
+    async def wrapper(client, message):
+        try:
+            if (message.from_user.is_self or
+               message.from_user.id in SUDOERS
+            ):
+                return await mystic(client, message)
+        except:
+            if (message.outgoing or
+               message.from_user.id in SUDOERS
+            ):
+                return await mystic(client, message)
+            
+    return wrapper
+    
+
+def cb_wrapper(func):
+    async def wrapper(bot, cb):
+        sudousers = SUDOERS
+        if (cb.from_user.id != app.me.id and
+            cb.from_user.id not in sudousers
+        ):
+            return await cb.answer(
+                "❎ You Are Not A Sudo User❗",
+                cache_time=0,
+                show_alert=True,
+            )
+        else:
+            try:
+                return await func(bot, cb)
+            except Exception:
+                print(format_exc())
+                return await cb.answer(
+                    f"❎ Something Went Wrong, Please Check Logs❗..."
+                )
+        
+    return wrapper
+
+
+def inline_wrapper(func):
+    from ... import __version__
+    async def wrapper(bot, query):
+        sudousers = SUDOERS
+        if (query.from_user.id != app.me.id and
+            query.from_user.id not in sudousers
+        ):
+            try:
+                button = [
+                    [
+                        InlineKeyboardButton(
+                            "💥 Deploy Shukla Userbot ✨",
+                            url=f"https://github.com/itzshukla/STRANGER-OPUSERBOT2.0"
+                        )
+                    ]
+                ]
+                await bot.answer_inline_query(
+                    query.id,
+                    cache_time=1,
+                    results=[
+                        (
+                            InlineQueryResultPhoto(
+                                photo_url=f"https://telegra.ph/file/7a2e0a67e160deb60e48b.jpg",
+                                title="🥀 Shukla Userbot ✨",
+                                thumb_url=f"https://telegra.ph/file/7a2e0a67e160deb60e48b.jpg",
+                                description=f"🌷 Deploy Your Own SHUKLAUSERBOT🌿...",
+                                caption=f"<b>🥀 Welcome » To » Shukla 🌷\n✅ Userbot {__version__} ✨...</b>",
+                                reply_markup=InlineKeyboardMarkup(button),
+                            )
+                        )
+                    ],
+                )
+            except Exception as e:
+                print(str(e))
+                await bot.answer_inline_query(
+                    query.id,
+                    cache_time=1,
+                    results=[
+                        (
+                            InlineQueryResultArticle(
+                                title="",
+                                input_message_content=InputTextMessageContent(
+                                    f"||**🥀 Please, Deploy Your Own Shukla Userbot❗...\n\nRepo:** <i>https://github.com/itzshukla/STRANGER-OPUSERBOT2.0/</i>||"
+                                ),
+                            )
+                        )
+                    ],
+                )
+            except Exception as e:
+                print(str(e))
+                pass
+        else:
+           return await func(bot, query)
+
+    return wrapper
